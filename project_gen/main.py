39480a340025f6f17f203e0b1f59bad3b51f09e0
Kristina Pylzy <kovengor@gmail.com>
2025-12-20 00:40:57 +0300

    Добавлена cli команда для скачивания openapi generator

--- project_gen/main.py
+++ project_gen/main.py
@@ -1,5 +1,6 @@
 import click
-from project_gen.utils.download import download
+from project_gen.utils.download import download, \
+    init
 from project_gen.utils.generate import generate
 
 
@@ -15,7 +16,15 @@ def cli() -> None:
 def generate_command() -> None:
     generate()
 
+
+@cli.command("init")
+def init_command() -> None:
+    init()
+
+
+
 cli.add_command(generate_command)
+cli.add_command(init_command)
 
 if __name__ == "__main__":
     cli()
--- project_gen/utils/download.py
+++ project_gen/utils/download.py
@@ -1,14 +1,16 @@
 import os
 import platform
 import shutil
-
 import requests
+
+
+OPENAPI_GENERATOR = "openapi-generator-cli-7.17.0.jar"
 
 def download() -> None:
     url = "https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.17.0/openapi-generator-cli-7.17.0.jar"
     with requests.get(url, stream=True, timeout=100, verify=False) as response:
         response.raise_for_status()
-        file_name = "openapi-generator-cli-7.17.0.jar"
+        file_name = OPENAPI_GENERATOR
         with open(file_name, "wb") as f:
             for chunk in response.iter_content(chunk_size=1024):
                 f.write(chunk)
@@ -17,3 +19,13 @@ def download() -> None:
             os.chmod(file_name, mode=0o755)
 
     shutil.move(file_name, f".venv/Scripts/{file_name}")
+
+
+
+def init() -> None:
+    if not os.path.exists(f".venv/Scripts/{OPENAPI_GENERATOR}"):
+        download()
+
+    print(f"Downloaded {OPENAPI_GENERATOR}")
+
+
