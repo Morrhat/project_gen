c398ea28a9193ab18a68437950dccff07db8686f
Kristina Pylzy <kovengor@gmail.com>
2025-12-25 15:59:50 +0300

    Updated download.py

--- project_gen/utils/download.py
+++ project_gen/utils/download.py
@@ -1,6 +1,5 @@
 import os
 import platform
-import shutil
 import requests
 
 
@@ -8,24 +7,37 @@ OPENAPI_GENERATOR = "openapi-generator-cli-7.17.0.jar"
 
 def download() -> None:
     url = "https://repo1.maven.org/maven2/org/openapitools/openapi-generator-cli/7.17.0/openapi-generator-cli-7.17.0.jar"
+
+    if platform.system() == "Windows":
+        scripts_dir = ".venv/Scripts"
+    else:
+        scripts_dir = ".venv/bin"  # На Mac/Linux
+
+    os.makedirs(scripts_dir, exist_ok=True)
+
     with requests.get(url, stream=True, timeout=100, verify=False) as response:
         response.raise_for_status()
         file_name = OPENAPI_GENERATOR
-        with open(file_name, "wb") as f:
+
+        file_path = os.path.join(scripts_dir, file_name)
+        with open(file_path, "wb") as f:
             for chunk in response.iter_content(chunk_size=1024):
                 f.write(chunk)
 
         if platform.system() != "Windows":
-            os.chmod(file_name, mode=0o755)
-
-    shutil.move(file_name, f".venv/Scripts/{file_name}")
-
+            os.chmod(file_path, mode=0o755)
 
 
 def init() -> None:
-    if not os.path.exists(f".venv/Scripts/{OPENAPI_GENERATOR}"):
+    if platform.system() == "Windows":
+        scripts_dir = ".venv/Scripts"
+    else:
+        scripts_dir = ".venv/bin"   # На Mac/Linux
+
+    file_path = os.path.join(scripts_dir, OPENAPI_GENERATOR)
+
+    if not os.path.exists(file_path):
         download()
 
     print(f"Downloaded {OPENAPI_GENERATOR}")
-
-
+    
